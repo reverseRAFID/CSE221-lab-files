b21c8ae8ff1552ba7fbab019850bccd1a09874e0
RAFID_KHAN <rafidbd9@gmail.com>
2023-10-13 17:09:11 +0600

    task1 v1

--- Fall 23 Lab2/task1.py
+++ Fall 23 Lab2/task1.py
@@ -6,11 +6,30 @@ num = int(num)
 arr = f_i.readline().strip().split(" ")
 arr = [int(i) for i in arr]
 flag = "IMPOSSIBLE"
-for j in range (rng):
-    for k in range (rng):
-        if arr[j] != arr[k] and arr[j] + arr[k] == num:
-            flag = f"{j+1} {k+1}"
+
+#solve with O(n^2) complexity
+for i in range(len(arr)):
+    for j in range(i+1, len(arr)):
+        if arr[i] + arr[j] == num:
+            flag = str(i+1) + " " + str(j+1)
             break
+        else:
+            continue
+    if flag != "IMPOSSIBLE":
+        break
+
+#solve with O(n) complexity
+i = 0
+j = rng-1
+while i < j:
+    if arr[i] + arr[j] == num:
+        flag = str(i+1) + " " + str(j+1)
+        break
+    elif arr[i] + arr[j] > num:
+        j -= 1
+    else:
+        i += 1
+
 f_o.write(flag)
 
 f_i.close()
