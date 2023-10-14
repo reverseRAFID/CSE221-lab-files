7d2a103af472df8a7569cd935e3d5fa68686ea26
RAFID_KHAN <rafidbd9@gmail.com>
2023-10-14 09:21:02 +0600

    updated

---
+++ Fall 23 Lab2/task2.py
@@ -0,0 +1,5 @@
+# f_i= open("D:\\rafid-console\CSE221-lab-files\Fall 23 Lab2\input2.txt", "r")
+# f_o= open("D:\\rafid-console\CSE221-lab-files\Fall 23 Lab2\output2.txt", "w")
+# rng, num = f_i.readline().strip().split(" ")
+
+print("Hello word")
---
+++ Fall 23 Lab3/task1.py
@@ -0,0 +1,46 @@
+def merge(a, b):
+# write your code here
+# Here a and b are two sorted list
+# merge function will return a sorted list after merging a and b
+
+    i = 0
+    j = 0
+    c = []
+    while i < len(a) and j < len(b):
+        if a[i] < b[j]:
+            c.append(a[i])
+            i+=1
+        else:
+            c.append(b[j])
+            j+=1
+    while i < len(a):
+        c.append(a[i])
+        i+=1
+    while j < len(b):
+        c.append(b[j])
+        j+=1
+    return c
+
+
+def mergeSort(arr):
+    if len(arr) <= 1:
+        return arr
+    else:
+        mid = len(arr)//2
+        a1 = mergeSort(arr[:mid])  # write the parameter 
+        a2 = mergeSort(arr[mid:])  # write the parameter
+        return merge(a1, a2)       # complete the merge function above 
+    
+
+f_input = open("D:\\rafid-console\\CSE221-lab-files\\Fall 23 Lab3\\input1.txt", "r")
+f_output = open("D:\\rafid-console\\CSE221-lab-files\\Fall 23 Lab3\\output1.txt", "w")
+var = f_input.readline().strip()
+arr = f_input.readline().strip().split(" ")
+for i in range(len(arr)):
+    arr[i] = int(arr[i])
+
+arr = mergeSort(arr)
+for i in range(len(arr)):
+    f_output.write(str(arr[i]) + " ")
+f_input.close()
+f_output.close()
